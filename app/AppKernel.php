1828e96385d106b6121e0828a25e3c01d36862f5
Jellibi Anas <jellibi_anas@hotmail.com>
2018-06-03 01:09:47 +0200

    install and configure FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new OC\PlatformBundle\OCPlatformBundle(),
             new OC\CoreBundle\OCCoreBundle(),
             new OC\UserBundle\UserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
